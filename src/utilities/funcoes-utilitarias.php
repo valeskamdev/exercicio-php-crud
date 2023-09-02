679b313206350aa9bbee32e22cf2d3b2445bbfe5
Valeska Marques <valeskkitty@gmail.com>
2023-09-02 12:22:13 -0300

    :bug: bug: resolvendo bug de casas decimais para média

--- src/utilities/funcoes-utilitarias.php
+++ src/utilities/funcoes-utilitarias.php
@@ -3,7 +3,7 @@
 function mediaAluno(float $nota_1, float $nota_2) : string
 {
     $media = ($nota_1 + $nota_2) / 2;
-    return number_format($media, 1);
+    return number_format($media, 2);
 }
 
 
